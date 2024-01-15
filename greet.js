08c10360774cce6498d843f9c1fafe2da78ccf22
Royce Calvin <royce20002006@yahoo.com>
2024-01-14 23:16:23 -0600

    finished

---
+++ greet.js
@@ -0,0 +1,14 @@
+// DO NOT MODIFY THE say FUNCTION
+function say(name) {
+  console.log(`${this.message} ${name}!`);
+}
+
+helloMessage = { message: 'Hello,' };
+heyThereMessage = { message: 'Hey there,' };
+
+// say(helloMessage, "John");
+// say(heyThereMessage, "Michael");
+
+
+say.apply(helloMessage, ['john']);
+say.apply(heyThereMessage,['Michael'])
